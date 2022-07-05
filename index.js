20475123955f4e699522e92dd4b0942cd7bf50a3
NotArtwork <AntArce3@gmail.com>
2022-07-05 10:23:13 -0400

    commit tree 87cc3275a62ad569acbba8609271c9c5daf22c3b

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (distance) => {
+    if (distance > 42) {
+        return distance - 42
+    } else if (distance < 42){
+        return 42 - distance
+    }
+}
+
+const distanceFromHqInFeet = (blocks) => {
+    return distanceFromHqInBlocks(blocks) * 264
+}
+
+const  distanceTravelledInFeet = (start, destination) => {   
+     
+
+    if (start < destination) {
+    return (destination - start) * 264
+    } else { 
+    return (start - destination) * 264
+    }
+}
+
+const calculatesFarePrice = (start, destination) => {   
+    
+    let distance = distanceTravelledInFeet(start, destination)
+
+    if (distance <= 400) {
+        return 0
+    }
+    else if (distance >= 400 && distance <= 2000) {
+        return .02 * (distance - 400) }
+    else if (distance >= 2000 && distance < 2500) {
+        return 25
+    }
+    else if (distance > 2500) {
+        return 'cannot travel that far'
+    }
+}
